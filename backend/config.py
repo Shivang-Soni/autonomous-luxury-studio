bdb3b60dc776d54a7a6a6ac78656917340383013
Shivang-Soni <soni.shivang.999@gmail.com>
2025-12-10 17:44:53 +0530

    Update config.py: load env variables

--- backend/config.py
+++ backend/config.py
@@ -1,1 +1,13 @@
 import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Configuration:
+    def __init__(self):
+        self.ANALYST_MODEL = os.getenv("ANALYST_MODEL", "")
+        self.ART_DIRECTOR_MODEL = os.getenv("ART_DIRECTOR_MODEL", "")
+        self.JUDGE_MODEL = os.getenv("JUDGE_MODEL", "")
+        self.PRODUCER_MODEL = os.getenv("PRODUCER_MODEL", "")
