5486cd32044c729e2f6eff525a3d6bf36794c799
Shivang-Soni <soni.shivang.999@gmail.com>
2025-12-10 17:50:17 +0530

    Update config.py: add GEMINI_API_KEY

--- backend/config.py
+++ backend/config.py
@@ -11,3 +11,4 @@ class Configuration:
         self.ART_DIRECTOR_MODEL = os.getenv("ART_DIRECTOR_MODEL", "")
         self.JUDGE_MODEL = os.getenv("JUDGE_MODEL", "")
         self.PRODUCER_MODEL = os.getenv("PRODUCER_MODEL", "")
+        self.GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")
